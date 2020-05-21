2810b4bfa05a28dc57f4cd51c745061438a92b34
npnelson <nnelson1@insight.rr.com>
2020-05-21 11:59:46 -0400

    Added registration extension method

--- src/NetToolBox.Messaging.AzureServiceBus/AzureMessagingFactoryServiceCollectionExtensions.cs
+++ src/NetToolBox.Messaging.AzureServiceBus/AzureMessagingFactoryServiceCollectionExtensions.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using NetToolBox.Messaging.Abstractions;
 
 namespace NetToolBox.Messaging.AzureServiceBus
 {
-    class AzureMessagingFactoryServiceCollectionExtensions
+    namespace Microsoft.Extensions.DependencyInjection
     {
+        public static class AzureMessagingFactoryServiceCollectionExtensions
+        {
+            public static IServiceCollection AddAzureMessaging(this IServiceCollection serviceCollection)
+            {
+                serviceCollection.TryAddSingleton<IMessagingFactory, AzureMessagingFactory>();
+                return serviceCollection;
+            }
+        }
     }
 }
